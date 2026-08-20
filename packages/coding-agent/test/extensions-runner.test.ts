5da37f2372e44deca10caef45810b7480696a796
darin <darinkishore@protonmail.com>
2026-08-20 16:45:07 -0700

    test(host): assimilate local contracts with v17.4

--- packages/coding-agent/test/extensions-runner.test.ts
+++ packages/coding-agent/test/extensions-runner.test.ts
@@ -3895,6 +3895,9 @@ describe("ExtensionRunner", () => {
 				commands: new Map(),
 				flags: new Map(),
 				shortcuts: new Map(),
+				busDisposers: [],
+				activateEventSubscriptions: () => {},
+				disposeEventSubscriptions: () => {},
 			};
 			return new ExtensionRunner([extension], new ExtensionRuntime(), tempDir.path(), sessionManager, modelRegistry);
 		};
